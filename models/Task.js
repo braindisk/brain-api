76371e3ed88fdfe27c0a2b74af39a8275bab9ebb
ankitsejwal <ankit@sejw.al>
2023-10-24 21:23:55 +0530

    fixed Task for ObjectId's

--- models/Task.js
+++ models/Task.js
@@ -3,12 +3,12 @@ import mongoose from 'mongoose';
 const taskSchema = new mongoose.Schema({
   title: { type: String, require: true },
   status: { type: String, enum: ['not-started', 'on-going', 'done'], default: 'not-started' },
-  project: { type: String, require: true },
+  project: { type: mongoose.Schema.Types.ObjectId(), ref: 'Project', require: true },
   priority: { type: Number, enum: [1, 2, 3] },
   sessions: [{ start: Date, end: Date }],
   elapsed_time: { type: Date },
   total_time: { type: Number },
-  created_by: { type: String },
+  created_by: { type: mongoose.Schema.Types.ObjectId(), ref: 'User', require: true },
   created_on: { type: Date, default: Date.now() },
   started_on: { type: Date },
   completed_on: { type: Date },
@@ -20,4 +20,3 @@ const Task = mongoose.model('Task', taskSchema);
 export default Task;
 
 // todo - turn on extension for escaping comma
-// type change - created_by, project

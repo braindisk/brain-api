7efaa68865c74ce304ffb895243a25525252ba41
ankitsejwal <ankit@sejw.al>
2023-10-26 19:39:47 +0530

    routes piping

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
-import login from './routes/login';
+import auth from './routes/auth';
 import tasks from './routes/tasks';
+import users from './routes/users';
+import projects from './routes/projects';
 import checkEnvVariables from './utils/checkEnvVariables';
 import dbConnect from './utils/dbConnect';
 
@@ -10,8 +12,10 @@ dbConnect();
 const app = express();
 
 app.use(express.json());
-app.use('/api/login', login);
+app.use('/api/auth', auth);
 app.use('/api/tasks', tasks);
+app.use('/api/users', users);
+app.use('/api/projects', projects);
 
 const port = 3000;
 app.listen(port, () => console.log(`server is running on port ${port}`));

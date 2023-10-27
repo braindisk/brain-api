e31a209500a5695529704452c7ec026f6b6d3e36
ankitsejwal <ankit@sejw.al>
2023-10-28 00:10:13 +0530

    finished auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import User from '../models/User';
+import genAuthToken from '../utils/genAuthToken';
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  res.json('login page');
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(404).json({ message: 'user not found' });
+  const match = await Bun.password.verify(req.body.password, user.password);
+  if (!match) return res.status(500).json({ message: 'Incorrect password' });
+  const token = genAuthToken(user);
+  res.status(200).header('braindisk-auth-token', token).json(user);
 });
 
 export default router;

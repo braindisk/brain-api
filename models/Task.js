c9321be2ed396306e28299eb6f790ad0c8171dbd
ankitsejwal <ankit@sejw.al>
2023-10-31 16:00:40 +0530

    update task schema

--- models/Task.js
+++ models/Task.js
@@ -5,6 +5,7 @@ Joi.objectid = joiObjectid(Joi);
 
 const taskSchema = new mongoose.Schema({
   title: { type: String, require: true },
+  description: { type: String },
   status: { type: String, enum: ['not-started', 'on-going', 'done'], default: 'not-started' },
   project: { type: mongoose.Schema.Types.ObjectId(), ref: 'Project', require: true },
   priority: { type: Number, enum: [1, 2, 3] },
@@ -22,10 +23,11 @@ export default mongoose.model('Task', taskSchema);
 
 const joiSchema = {
   title: Joi.string().required(),
+  description: Joi.string().optional(),
   status: Joi.string().valid('not-started', 'on-going', 'done').optional(),
   project: Joi.objectid().required(),
-  priority: Joi.number().valid(1, 2, 3),
-  sessions: Joi.date(),
+  priority: Joi.number().valid(1, 2, 3).optional(),
+  sessions: Joi.array().items(Joi.object({ start: Joi.date(), end: Joi.date() })),
   elapsedTime: Joi.date(),
   totalTime: Joi.date(),
   createdBy: Joi.objectid().required(),

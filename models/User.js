12109d5948fc4de12ea832e84c0604923354d161
ankitsejwal <ankit@sejw.al>
2023-11-18 20:10:37 +0530

    updated User schema for Projects

--- models/User.js
+++ models/User.js
@@ -3,11 +3,18 @@ import Joi from 'joi';
 import joiObjectid from 'joi-objectid';
 Joi.objectid = joiObjectid(Joi);
 
+const projectSchema = new mongoose.Schema({
+  title: { type: String, min: 3, max: 20, required: true },
+  isActive: { type: Boolean, default: true },
+  createdDate: { type: Date, default: Date.now(), required: true },
+});
+
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true, min: 3, max: 20 },
   lastName: { type: String, required: true, min: 3, max: 20 },
   email: { type: String, required: true, min: 5, max: 50 },
   password: { type: String, required: true, min: 6, max: 100 },
+  projects: [projectSchema],
 });
 
 export default mongoose.model('User', userSchema);
@@ -17,6 +24,7 @@ const joiSchema = {
   lastName: Joi.string().min(3).max(20).required(),
   email: Joi.string().email().min(3).max(50).required(),
   password: Joi.string().min(6).max(100).required(),
+  projects: Joi.array().items({ title: Joi.string().min(3).max(20).required(), isActive: Joi.boolean() }),
 };
 
 export { joiSchema };
